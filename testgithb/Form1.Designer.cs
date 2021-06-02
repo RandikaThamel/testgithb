2f19418b76d37e861dfe2d2b3137a471b2146514
randikathamel <randikasu2009@gmail.com>
2021-06-02 15:41:22 +0530

    button1 added

--- testgithb/Form1.Designer.cs
+++ testgithb/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace testgithb
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(412, 82);
+            this.button1.Location = new System.Drawing.Point(35, 51);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
